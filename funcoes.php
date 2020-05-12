956f7e0c38b89a24e666425eb35227837b5ee871
Samuel Caladas <samuel.caldas@gmail.com>
2020-05-12 15:16:26 -0300

    Initial commit

---
+++ funcoes.php
@@ -0,0 +1,15 @@
+﻿<?php #Diversos#
+	//conta array
+	function contar($array){return count($array);};
+#Funções de navegação
+	//Historico
+	function historico($go){echo "<script>history.go(".$go.");</script>";}
+	//Redireciona
+	function redirecionar($pagina){echo"<meta http-equiv='refresh' content='0;URL=".$pagina."' />";}
+	//Alerta
+	function alerta($msg){echo "<script>alert('".$msg."');</script>";}
+	//Eerro
+	function myErro($erro){alerta($erro);historico("-1");}
+	//Sucesso
+	function sucesso($pagina){alerta("Sucesso!");historico("-1");}
+?>
